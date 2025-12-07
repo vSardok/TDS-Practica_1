814c8c750a92af14d2e2011950673df62ec6effb
Luis Pelayo <luispelayo16@hotmail.com>
2025-12-07 09:08:49 -0600

    Ajustes finales para .env

--- project/settings.py
+++ project/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -204,9 +205,9 @@ EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.Email
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'example@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'xxxx xxxx xxxx xxxx')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'example@gmail.com')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', '')
 
 print("✅ Configuración de correo electrónico cargada., EMAIL_HOST_USER:", EMAIL_HOST_USER, "EMAIL_BACKEND:", EMAIL_BACKEND, "EMAIL_HOST:", EMAIL_HOST, "EMAIL_HOST_PASSWORD:", EMAIL_HOST_PASSWORD)
 
